b115c65159de5ece6db219240811a261756c3e9a
A. Akbar Hidayat <keripix@gmail.com>
2013-09-17 20:35:52 +0700

    added control

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,21 @@
 ;(function(global){
 
   var fabric = global.fabric,
-      canvas = new fabric.Canvas("demo");
+      canvas = new fabric.Canvas("demo"),
+      brushColor = document.getElementById("brushColor"),
+      brushSize = document.getElementById("brushSize");
 
   canvas.isDrawingMode = true;
   canvas.freeDrawingBrush = new fabric.SquareBrush(canvas);
+  canvas.freeDrawingBrush.width = brushSize.value;
+  canvas.freeDrawingBrush.color = brushColor.value;
+
+  brushSize.addEventListener("change", function(e){
+    canvas.freeDrawingBrush.width = parseInt(e.target.value, 10);
+  }, false);
+
+  brushColor.addEventListener("change", function(e){
+    canvas.freeDrawingBrush.color = e.target.value;
+  }, false);
 
 })(this);
